3133bf1dcdc38caa78310a412787befe5d8f312b
Guilherme Tavares <guigo211103@gmail.com>
2022-03-28 22:37:17 -0300

    1st commit
    Primeiro commit da aplicação / feito o header

---
+++ src/components/header/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+    background: var(--purple);
+`
+
+export const Content = styled.header`
+    color: var(--text-title);
+    max-width: 1120px;
+    margin: 0 auto;
+
+    padding: 2rem 1rem 12rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
---
+++ src/styles/global.ts
@@ -0,0 +1,51 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    :root{
+        --background: #121212;
+        --shape: #272727;
+        --text-title: #fff;
+        --text-body: #cac5d9;
+        --purple: #57007E;
+    }
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html{
+        // font-size: 16px; //default (desktop)
+        @media (max-width: 1000px) {
+            font-size: 93.75%; //15px
+        }
+        @media (max-width: 720px) {
+            font-size: 87.5%; //14px
+        }
+    }
+
+    body{ 
+        background-color: var(--background);
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button{
+        font-family: 'Quicksand', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong{
+        font-weight: 600;
+    }
+
+    button{
+        cursor: pointer;
+    }
+
+
+    [disabled]{
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+`;
